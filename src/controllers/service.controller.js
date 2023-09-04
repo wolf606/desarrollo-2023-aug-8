e5c40a4451954061e683893fcf598f520f1d68c9
wolf606 <luismsanchez808@gmail.com>
2023-09-04 00:02:55 -0500

    Implement Update and Delete methods for Services

--- src/controllers/service.controller.js
+++ src/controllers/service.controller.js
@@ -75,14 +75,19 @@ async function update(req, res) {
                 };
             }
         );
-        newPics.pics = pics;
+        newPics.$push = { photos: { $each: pics } };
     }
-    console.debug("picsA: ", newPics.pics);
     if (req.body.active !== undefined) dict.active = req.body.active;
     if (req.body.category !== undefined) dict.category = req.body.category;
 
-    Service.findByIdAndUpdate({ _id: params.id }, {dict, $push: { $each: newPics.pics }}, { new: true })
-    .then((ser) => {
+    Service.findByIdAndUpdate(
+        { _id: params.id }, 
+        {
+            $set: dict,
+            ...newPics
+        }, 
+        { new: true }
+    ).then((ser) => {
         if (ser === null) {
             res.status(404).send({ error: "User not found." });
         } else {
@@ -95,9 +100,54 @@ async function update(req, res) {
     });
 }
 
+async function destroy(req, res) {
+    const params = req.params;
+    Service.findByIdAndDelete({ _id: params.id })
+    .then((ser) => {
+        if (ser === null) {
+            res.status(404).send({ error: "Service not found." });
+        } else {
+            res.status(200).send(ser);
+        }
+    })
+    .catch((err) => {
+        res.status(422).send({ error: "Cannot delete Service. Reason: "});
+        console.debug(err);
+    });
+}
+
+async function wipe(req, res) {
+    const params = req.params;
+    var dict = {};
+    if (req.body.id !== undefined) dict._id = req.body.id;
+    if (req.body.name !== undefined) dict.name = req.body.name;
+    if (req.body.description !== undefined) dict.description = req.body.description;
+    if (req.body.active !== undefined) dict.active = req.body.active;
+    if (req.body.category !== undefined) dict.category = req.body.category;
+    
+    if (dict !== null && Object.keys(dict).length > 0){
+        Service.deleteMany(dict)
+        .then((ser) => {
+            if (ser === null) {
+                res.status(404).send({ error: "Services not found." });
+            } else {
+                res.status(200).send(ser);
+            }
+        })
+        .catch((err) => {
+            res.status(422).send({ error: "Cannot delete Services. Reason: "});
+            console.debug(err);
+        });
+    } else {
+        res.status(422).send({ error: "Cannot delete services. No params were sent."});
+    }
+}
+
 module.exports = {
     index,
     show,
     store,
-    update
+    update,
+    destroy,
+    wipe
 }
--- src/routes/service.route.js
+++ src/routes/service.route.js
@@ -3,17 +3,22 @@ const api = express.Router();
 const multer = require("multer");
 const storageLoc = 'uploads/service/';
 const upload = multer({ dest: storageLoc });
+const { ensureAuth } = require("../middleware/user.auth");
 
 const {
     show,
     index,
     store,
-    update
+    update,
+    destroy,
+    wipe
 } = require("../controllers/service.controller");
 
-api.post(`/`, upload.array('gallery'), store);
+api.post(`/`, upload.array('gallery'), ensureAuth, store);
 api.get('/', index);
 api.get('/:id', show);
-api.put('/:id', upload.array('gallery'), update);
+api.put('/:id', upload.array('gallery'), ensureAuth, update);
+api.delete('/:id', ensureAuth, destroy);
+api.delete('/', ensureAuth, wipe);
 
 module.exports = api;
